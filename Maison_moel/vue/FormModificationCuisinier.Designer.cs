5cee64cb952d302a1ba165f77f3d776d299a7b31
Akzamov Raphaël <akzamov.rl@gmail.com>
2025-01-10 16:45:30 +0100

    Ajout des From de modification du Personnel

---
+++ Maison_moel/vue/FormModificationCuisinier.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Maison_moel.vue
+{
+    partial class FormModificationCuisinier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FormModificationCuisinier
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.gradient_1500_1500_1_;
+            ClientSize = new Size(994, 518);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormModificationCuisinier";
+            Text = "FormModificationCuisinier";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
