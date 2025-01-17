5bade3552fef68d0b9f71fabfd85ef8c6fa1a91e
Akzamov Raphaël <akzamov.rl@gmail.com>
2025-01-17 16:56:49 +0100

    modif form modif personnel

--- Maison_moel/vue/FormModificationCuisinier.Designer.cs
+++ Maison_moel/vue/FormModificationCuisinier.Designer.cs
@@ -40,28 +40,28 @@
             // 
             // textBox_nom
             // 
-            textBox_nom.Location = new Point(208, 123);
+            textBox_nom.Location = new Point(273, 128);
             textBox_nom.Name = "textBox_nom";
             textBox_nom.Size = new Size(202, 23);
             textBox_nom.TabIndex = 0;
             // 
             // textBox_prenom
             // 
-            textBox_prenom.Location = new Point(208, 162);
+            textBox_prenom.Location = new Point(273, 167);
             textBox_prenom.Name = "textBox_prenom";
             textBox_prenom.Size = new Size(202, 23);
             textBox_prenom.TabIndex = 1;
             // 
             // textBox_email
             // 
-            textBox_email.Location = new Point(208, 201);
+            textBox_email.Location = new Point(273, 206);
             textBox_email.Name = "textBox_email";
             textBox_email.Size = new Size(202, 23);
             textBox_email.TabIndex = 2;
             // 
             // dtp_datenaissance
             // 
-            dtp_datenaissance.Location = new Point(208, 241);
+            dtp_datenaissance.Location = new Point(273, 246);
             dtp_datenaissance.Name = "dtp_datenaissance";
             dtp_datenaissance.Size = new Size(202, 23);
             dtp_datenaissance.TabIndex = 3;
@@ -69,36 +69,44 @@
             // label_dateNaiss
             // 
             label_dateNaiss.AutoSize = true;
+            label_dateNaiss.BackColor = Color.Transparent;
+            label_dateNaiss.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
             label_dateNaiss.Location = new Point(80, 247);
             label_dateNaiss.Name = "label_dateNaiss";
-            label_dateNaiss.Size = new Size(103, 15);
+            label_dateNaiss.Size = new Size(161, 20);
             label_dateNaiss.TabIndex = 4;
             label_dateNaiss.Text = "Date de Naissance";
             // 
             // label_nom
             // 
             label_nom.AutoSize = true;
+            label_nom.BackColor = Color.Transparent;
+            label_nom.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
             label_nom.Location = new Point(80, 131);
             label_nom.Name = "label_nom";
-            label_nom.Size = new Size(34, 15);
+            label_nom.Size = new Size(45, 20);
             label_nom.TabIndex = 5;
             label_nom.Text = "Nom";
             // 
             // label_prenom
             // 
             label_prenom.AutoSize = true;
+            label_prenom.BackColor = Color.Transparent;
+            label_prenom.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
             label_prenom.Location = new Point(80, 170);
             label_prenom.Name = "label_prenom";
-            label_prenom.Size = new Size(49, 15);
+            label_prenom.Size = new Size(70, 20);
             label_prenom.TabIndex = 6;
             label_prenom.Text = "Prénom";
             // 
             // label_email
             // 
             label_email.AutoSize = true;
+            label_email.BackColor = Color.Transparent;
+            label_email.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
             label_email.Location = new Point(80, 209);
             label_email.Name = "label_email";
-            label_email.Size = new Size(36, 15);
+            label_email.Size = new Size(53, 20);
             label_email.TabIndex = 7;
             label_email.Text = "Email";
             // 
--- Maison_moel/vue/FormModificationServeur.Designer.cs
+++ Maison_moel/vue/FormModificationServeur.Designer.cs
@@ -28,7 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label_email = new Label();
+            label_prenom = new Label();
+            label_nom = new Label();
+            label_dateNaiss = new Label();
+            dtp_datenaissance = new DateTimePicker();
+            textBox_email = new TextBox();
+            textBox_prenom = new TextBox();
+            textBox_nom = new TextBox();
             SuspendLayout();
+            // 
+            // label_email
+            // 
+            label_email.AutoSize = true;
+            label_email.BackColor = Color.Transparent;
+            label_email.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label_email.Location = new Point(106, 226);
+            label_email.Name = "label_email";
+            label_email.Size = new Size(53, 20);
+            label_email.TabIndex = 15;
+            label_email.Text = "Email";
+            // 
+            // label_prenom
+            // 
+            label_prenom.AutoSize = true;
+            label_prenom.BackColor = Color.Transparent;
+            label_prenom.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label_prenom.Location = new Point(106, 187);
+            label_prenom.Name = "label_prenom";
+            label_prenom.Size = new Size(70, 20);
+            label_prenom.TabIndex = 14;
+            label_prenom.Text = "Prénom";
+            // 
+            // label_nom
+            // 
+            label_nom.AutoSize = true;
+            label_nom.BackColor = Color.Transparent;
+            label_nom.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label_nom.Location = new Point(106, 148);
+            label_nom.Name = "label_nom";
+            label_nom.Size = new Size(45, 20);
+            label_nom.TabIndex = 13;
+            label_nom.Text = "Nom";
+            // 
+            // label_dateNaiss
+            // 
+            label_dateNaiss.AutoSize = true;
+            label_dateNaiss.BackColor = Color.Transparent;
+            label_dateNaiss.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label_dateNaiss.Location = new Point(106, 264);
+            label_dateNaiss.Name = "label_dateNaiss";
+            label_dateNaiss.Size = new Size(161, 20);
+            label_dateNaiss.TabIndex = 12;
+            label_dateNaiss.Text = "Date de Naissance";
+            // 
+            // dtp_datenaissance
+            // 
+            dtp_datenaissance.Location = new Point(299, 263);
+            dtp_datenaissance.Name = "dtp_datenaissance";
+            dtp_datenaissance.Size = new Size(202, 23);
+            dtp_datenaissance.TabIndex = 11;
+            // 
+            // textBox_email
+            // 
+            textBox_email.Location = new Point(299, 223);
+            textBox_email.Name = "textBox_email";
+            textBox_email.Size = new Size(202, 23);
+            textBox_email.TabIndex = 10;
+            // 
+            // textBox_prenom
+            // 
+            textBox_prenom.Location = new Point(299, 184);
+            textBox_prenom.Name = "textBox_prenom";
+            textBox_prenom.Size = new Size(202, 23);
+            textBox_prenom.TabIndex = 9;
+            // 
+            // textBox_nom
+            // 
+            textBox_nom.Location = new Point(299, 145);
+            textBox_nom.Name = "textBox_nom";
+            textBox_nom.Size = new Size(202, 23);
+            textBox_nom.TabIndex = 8;
             // 
             // FormModificationServeur
             // 
@@ -36,12 +116,30 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.gradient_1500_1500_1_;
             ClientSize = new Size(994, 518);
+            Controls.Add(label_email);
+            Controls.Add(label_prenom);
+            Controls.Add(label_nom);
+            Controls.Add(label_dateNaiss);
+            Controls.Add(dtp_datenaissance);
+            Controls.Add(textBox_email);
+            Controls.Add(textBox_prenom);
+            Controls.Add(textBox_nom);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FormModificationServeur";
             Text = "FormModificationServeur";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label_email;
+        private Label label_prenom;
+        private Label label_nom;
+        private Label label_dateNaiss;
+        private DateTimePicker dtp_datenaissance;
+        private TextBox textBox_email;
+        private TextBox textBox_prenom;
+        private TextBox textBox_nom;
     }
 }
